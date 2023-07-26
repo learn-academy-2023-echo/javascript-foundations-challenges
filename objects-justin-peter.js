9f42c30d18e5f7bd8077d5b84f96ce25734b3ebe
peter ji <hyeongjun9@gmail.com>
2023-07-26 15:49:04 -0700

    objects challenges

--- objects-justin-peter.js
+++ objects-justin-peter.js
@@ -31,6 +31,7 @@ const product = {
   name: "chair",
   price: 24.99
 }
+
 // Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
 
 const describeProduct = (object) => {
@@ -42,10 +43,10 @@ const describeProduct = (object) => {
 // Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
 
 const totalWithTax = (object) => {
-    return (object.price * 1.07).toFixed(2)
+    return Number((object.price * 1.07).toFixed(2))
 }
 
-//  console.log(totalWithTax(product))
+ // console.log(totalWithTax(product))
 
 
 // Consider this variable:
@@ -55,7 +56,7 @@ const lunch = {
   type: "sandwich",
   ingredients: ["bread", "peanut butter", "banana"],
   recipe: function () { 
-    return ` The ingredients for a ${this.name} ${this.type} are ${this.ingredients[0]}, ${this.ingredients[1]}, and ${this.ingredients[2]}.`
+    return `The ingredients for a ${this.name} ${this.type} are ${this.ingredients[0]}, ${this.ingredients[1]}, and ${this.ingredients[2]}.`
   }
 }
 // Write the code that accesses the ingredients property.
@@ -69,7 +70,7 @@ const lunch = {
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
 
 // const recipe = (object) => {
-//     return ` The ingredients for a ${object.name} ${object.type} are ${object.ingredients[0]}, ${object.ingredients[1]}, and ${object.ingredients[2]}.`
+//     return `The ingredients for a ${object.name} ${object.type} are ${object.ingredients[0]}, ${object.ingredients[1]}, and ${object.ingredients[2]}.`
 // }
 
 // console.log(recipe(lunch))
@@ -106,50 +107,98 @@ const onlyNames = (array) => {
 const oldAnimals = (array) => {
     return array.filter((object) => object.age > 10).map((object) => object.name)
 }
-console.log(oldAnimals(animals))
+// console.log(oldAnimals(animals))
+
 // Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+
+const sentence = (array) => {
+    return array.map((object) => `${object.name} is a ${object.age} year old ${object.type}`)
+}
+// console.log(sentence(animals))
+
 // Consider this variable:
 
-// const author = {
-//   name: "H. G. Wells",
-//   genre: "science fiction"
-// }
+const author = {
+  name: "H. G. Wells",
+  genre: "science fiction"
+}
+
 // Write the code that destructures the author object so that the following code snippet will run successfully:
+
+const {name, genre} = author
 // console.log(`${name} is a ${genre} author`)
 // // output: "H. G. Wells is a science fiction author"
+
 // Consider this variable:
 
-// const pokeOne = {
-//   species: "Charmandar",
-//   pokemon_type: "Fire"
-// }
+const pokeOne = {
+  species: "Charmandar",
+  pokemon_type: "Fire"
+}
 
-// const pokeTwo = {
-//   species: "Magikarp",
-//   pokemon_type: "Water"
-// }
+const pokeTwo = {
+  species: "Magikarp",
+  pokemon_type: "Water"
+}
+
 // Create a function called describePokemon that take an object like the ones above and uses destructuring to return a description of the Pokemon so that the following code snippet will run successfully:
+
+const describePokemon = (object) => {
+  return `${species} is a ${pokemon_type} pokemon`
+}
+
+// const {species, pokemon_type} = pokeOne
 // console.log(describePokemon(pokeOne))
 // // output: "Charmandar is a Fire pokemon"
+
+// const {species, pokemon_type} = pokeTwo
 // console.log(describePokemon(pokeTwo))
 // // output: "Magikarp is a Water pokemon"
+
 // Consider this variable:
 
-// const triangleDimensions = {
-//   base: 2,
-//   height: 5
-// }
+const triangleDimensions = {
+  base: 2,
+  height: 5,
+  area: function () {
+    return this.base * this.height * 0.5
+  }
+}
+
 // Modify the triangleDimensions object to have a method that returns the area of the triangle.
+// console.log(triangleDimensions.area())
+
 // Write the code that will update the base to be the value of 6.
+triangleDimensions.base = 6
+// console.log(triangleDimensions)
+
 // 🏔 Stretch Goals
 // Consider this variable:
 
-// const learn = {
-//   cohorts: {
-//     2022: ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel"],
-//     2023: ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliet"]
-//   }
-// }
+const learn = {
+  cohorts: {
+    2022: ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel"],
+    2023: ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliet"]
+  }
+}
+
 // Write the code that logs the name of your cohort.
+
+// console.log(learn.cohorts['2023'][4])
+
 // Write the code that uses destructuring to log the name of your cohort.
+
+const {cohorts} = learn
+
+//console.log(cohorts['2023'][4])
+
 // Create a function that takes an object like the one above and returns an array with a string of every cohort name and year.
+
+const nameYear = (object) => {
+   let arr1 = object.cohorts['2022'].map((value) => `2022 ${value}`)
+   let arr2 = object.cohorts['2023'].map((value) => `2023 ${value}`)
+   return arr1.concat(arr2)
+}
+
+// console.log(nameYear(learn))
+// output: ["2022 Alpha", "2022 Bravo", "2022 Charlie", "2022 Delta", "2022 Echo", "2022 Foxtrot", "2022 Golf", "2022 Hotel", "2023 Alpha", "2023 Bravo", "2023 Charlie", "2023 Delta", "2023 Echo", "2023 Foxtrot", "2023 Golf", "2023 Hotel", "2023 India", "2023 Juliet"]
